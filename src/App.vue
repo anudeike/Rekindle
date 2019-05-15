fc663d98e94b4e33fc781a6b5902ba2b9bc63302
anudeike <ikechukwuanude@gmail.com>
2019-05-15 01:18:07 -0500

    created files and started work on the form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
+
+      <v-toolbar dark color="primary">
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+
+        <!--normally the logo would go here-->
+        <v-toolbar-title class="logo">Rekindle</v-toolbar-title>
+
+        <v-toolbar-title>home</v-toolbar-title>
+        <v-toolbar-title>browse</v-toolbar-title>
+        <v-toolbar-title>random</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+
+        <v-toolbar-title>ikechukwu</v-toolbar-title>
+        <v-btn round color="primary" dark> new </v-btn>
+
+      </v-toolbar>
+
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/new">New</router-link>
+
+
+
     </div>
     <router-view />
   </div>
@@ -28,4 +53,9 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+#logo {
+  font-family: "SF Pro Display", sans-serif;
+}
+
 </style>
---
+++ src/views/newStory.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+
+    <!--contains the main content that the user will type and submit their stuff into-->
+    <div class="content">
+      <v-form ref="form">
+        <v-text-field v-model="model" label="Title"></v-text-field>
+      </v-form>
+
+      <v-textarea name="input-7-1" label="Default style" value="nigga" hint="Hint text"></v-textarea>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+  .content{
+    margin: 20px;
+
+  }
+</style>
