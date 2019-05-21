032e2e1e409623bb56451244a2f657a346b046fb
anudeike <ikechukwuanude@gmail.com>
2019-05-21 08:55:22 -0500

    added firebase not yet secure, need to put credentials in process files

--- src/main.js
+++ src/main.js
@@ -10,6 +10,22 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 
+//firebase config
+// Your web app's Firebase configuration - in production make sure to get rid of this and put
+//in a process file or some security file
+var firebaseConfig = {
+  apiKey: "AIzaSyACyXCdRKkIy3EhFZnzWfpBDSaI87vFxTI",
+  authDomain: "rekindle-f3fdc.firebaseapp.com",
+  databaseURL: "https://rekindle-f3fdc.firebaseio.com",
+  projectId: "rekindle-f3fdc",
+  storageBucket: "rekindle-f3fdc.appspot.com",
+  messagingSenderId: "951035308176",
+  appId: "1:951035308176:web:dbd0dd4ece068c1b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.use(BootstrapVue);
 Vue.use(Vuetify);
 Vue.use(VueResource);
